6e5f56312977c887b219bd46b4ca205cab8b26f2
RHSeeger <RHSeeger@gmail.com>
2025-12-14 12:40:19 -0500

    Added a GameStats class to hold all information about a game (they're scattered at the moment)

---
+++ src/typescript/gameState.ts
@@ -0,0 +1,13 @@
+
+import type { PlayerState } from "./playerState";
+import type { Unlocks } from "./unlocks";
+import type { Achievement } from "./achievement";
+import type { GameStats } from "./gameStats";
+
+export type GameState = {
+  humanPlayer: PlayerState;
+  computerPlayer: PlayerState;
+  unlockedUnlocks: Unlocks[];
+  accomplishedAchievements: Achievement[];
+  gameStats: GameStats;
+};
